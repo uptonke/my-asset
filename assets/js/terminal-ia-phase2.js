7471332f79868308bff81bcd7b237796399f4d57
uptonke <uptonke7@gmail.com>
2026-06-07 12:29:06 +0800

    Delete assets directory

--- assets/js/terminal-ia-phase2.js
+++
@@ -1,6 +0,0 @@
-// terminal-ia-phase2.js
-// Safe no-op placeholder.
-// index.html references this file; this avoids a 404 while keeping IA panels neutralized.
-(function () {
-  window.__terminalIAPhase2Loaded = true;
-})();
